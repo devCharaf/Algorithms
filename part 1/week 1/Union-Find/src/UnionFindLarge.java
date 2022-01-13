2f57f54bb8d4de8436b2c41927d8e6fbdc8fa467
Charaf <50533653+devCharaf@users.noreply.github.com>
2022-01-13 12:39:06 -0500

    Update UnionFindLarge.java

--- part 1/week 1/Union-Find/src/UnionFindLarge.java
+++ part 1/week 1/Union-Find/src/UnionFindLarge.java
@@ -1,3 +1,12 @@
+/*
+Add a method find() to the union-find data type so that find(i) returns the largest element in the connected component containing i.
+
+The operations, union(), connected(), and find() should all take logarithmic time or better.
+
+For example, if one of the connected components is {1,2,6,9}, then the find() method should return 9 for each of the four elements in the connected components.
+*/
+
+
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Arrays;
